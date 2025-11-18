21b26fb73d79a70fcce33a5daab7604c044b5ac5
Dhruv Kandula <dhruv.kandula@gmail.com>
2025-11-17 23:31:32 -0800

    Refactored AST, cleaner type structure

--- impls/go/ast.go
+++ impls/go/ast.go
@@ -1,31 +1,29 @@
 package main
 
+// SExpr can either be an atom or a list of SExprs
+type SExpr interface {
+	isSExpr()
+}
+
+type List struct {
+	values []SExpr
+}
+
+func (List) isSExpr() {}
+
+// Atom is an SExpr, and can either be a Symbol or a Number
+type Atom interface {
+	SExpr
+	isAtom()
+}
+
 type Symbol struct {
-	sym string
+	data string
 }
 
-type AtomType string
-
-const (
-	AtomSymbol AtomType = "symbol"
-	AtomNumber AtomType = "number"
-)
-
-type Atom struct {
-	typ    AtomType
-	symbol Symbol
-	number float64
+type Number struct {
+	data float64
 }
 
-type SexprType string
-
-const (
-	SexprList SexprType = "list"
-	SexprAtom SexprType = "atom"
-)
-
-type Sexpr struct {
-	typ  SexprType
-	list []Sexpr
-	atom Atom
-}
+func (Symbol) isAtom() {}
+func (Number) isAtom() {} // defines the Number as part of an atom
